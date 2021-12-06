040e58e6cc540e93c0221d2a44ddfb27a0674c9a
Natali-Skv <nat-s.skv@mail.ru>
2021-12-06 20:49:26 +0300

    creating handle_request()  in not auth mode in process

--- project/server/workers/include/request_handler_not_auth.h
+++ project/server/workers/include/request_handler_not_auth.h
@@ -7,6 +7,6 @@ class RequestHandlerNotAuth{
 public:
     bool handle_request( HttpRequest& request, HttpResponse& response);
 private:
-    bool download_file(const std::string& filename, const std::string & file, HttpResponse& response);
-    bool upload_file(const std::string& file_id, const std::string &optional_password, HttpResponse& response);
+    bool download_file(const std::string& file_id, const std::string &opt_pswd, HttpResponse& response);
+    bool upload_file(const std::string& filename, const std::string & file, HttpResponse& response);
 };
--- project/server/workers/src/request_handler_not_auth.cpp
+++ project/server/workers/src/request_handler_not_auth.cpp
@@ -1,12 +1,13 @@
 #include <request_handler_not_auth.h>
 #include <http_status_code.h>
+#include <data_base.h>
 
 bool
 RequestHandlerNotAuth::handle_request(HttpRequest &request, HttpResponse &response) {
-    if ( request.get_headers()["command"] == "download") {
+    if (request.get_headers()["command"] == "download") {
         return RequestHandlerNotAuth::download_file(request.get_headers()["filename"], request.get_body(), response);
     }
-    if ( request.get_headers()["command"] == "upload") {
+    if (request.get_headers()["command"] == "upload") {
         return RequestHandlerNotAuth::upload_file(request.get_headers()["filename"], request.get_headers()["password"],
                                                   response);
     }
@@ -15,15 +16,30 @@ RequestHandlerNotAuth::handle_request(HttpRequest &request, HttpResponse &respon
     return false;
 }
 
-bool RequestHandlerNotAuth::download_file(const std::string &filename, const std::string &file,
+bool RequestHandlerNotAuth::download_file(const std::string &file_id, const std::string &opt_pswd,
                                           HttpResponse &response) {
 
-    //обращение к бд
-    // обработка ошибок бд
+    try {
+        DataBase db_worker("postgres", "postgres", "5432", "localhost", ""); // TODO: тут не может быть исключений?
+        if (!db_worker.not_auth_mode.has_access_on_unauth_user_file(file_id, opt_pswd)) {
+            response = HttpResponse({}, {}, 0, 0, HttpStatusCode::BadRequest,
+                                    get_message(HttpStatusCode::BadRequest)); // TODO: удалить мажор минор
+            return false;
+        }
+    } catch (const std::string &error_msg) {
+        //TODO: вывод в лог error_msg
+        response = HttpResponse({}, {}, 0, 0, HttpStatusCode::BadRequest,
+                                get_message(HttpStatusCode::BadRequest)); // TODO: удалить мажор минор
+        return false;
+    }
+
+
+
+
     // обращение к ФС
     // обработка ошибок ФС + удаление из БД если надо
     response = HttpResponse({}, {}, request.get_major(), request.get_minor(), HttpStatusCode::BadRequest,
-                            get_message(HttpStatusCode::BadRequest));
+                            get_message(HttpStatusCode::BadRequest)); // TODO: удалить мажор минор
     return false;
 }
 
