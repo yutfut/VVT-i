9a6e81c4c091386e09bb4aa3250b526bc758824c
yut_fut <seregey.golubev@mail.ru>
2021-12-08 00:21:10 +0300

    edited gtest

--- test/gtest.cpp
+++ test/gtest.cpp
@@ -8,10 +8,9 @@
 #include "command.h"
 #include "validation.h"
 #include "http_request.h"
-#include "http_response.h"
 
 TEST(VALIDATION_TEST, VALIDATION_EMAIL_TEST_OK) {
-    EXPECT_EQ(Validator::validate_email("command"), 0);
+    EXPECT_EQ(Validator::validate_email("m@m.m"), 0);
 }
 
 TEST(VALIDATION_TEST, VALIDATION_EMAIL_TEST_FAIL) {
@@ -19,7 +18,7 @@ TEST(VALIDATION_TEST, VALIDATION_EMAIL_TEST_FAIL) {
 }
 
 TEST(VALIDATION_TEST, VALIDATION_PASSWORD_TEST_OK) {
-    EXPECT_EQ(Validator::validate_password("command"), 0);
+    EXPECT_EQ(Validator::validate_password("Qw12##12"), 0);
 }
 
 TEST(VALIDATION_TEST, VALIDATION_PASSWORD_TEST_FAIL) {
@@ -44,7 +43,7 @@ TEST(VALIDATION_TEST, VALIDATION_DIRECTORY_TEST_FAIL) {
 
 TEST(REGISTRATION_TEST, REGISTRATION_TEST_OK) {
     User user;
-    EXPECT_EQ(user.register_user("command"), 0);
+    EXPECT_EQ(user.register_user("m@m.m Qw12##12"), 0);
 }
 
 TEST(REGISTRATION_TEST, REGISTRATION_TEST_FAIL) {
@@ -54,13 +53,13 @@ TEST(REGISTRATION_TEST, REGISTRATION_TEST_FAIL) {
 
 TEST(REGISTRATION_TEST, LOGIN_TEST_OK) {
     User user;
-    EXPECT_EQ(user.login("command"), 0);
+    EXPECT_EQ(user.login("m@m.m Qw12##12"), 0);
 }
 
 TEST(REGISTRATION_TEST, LOGIN_TEST_OK_WHERE_USER_LOGINED) {
     User user;
     user.login("command");
-    EXPECT_EQ(user.login("command"), 0);
+    EXPECT_EQ(user.login("m@m.m Qw12##12"), 0);
 }
 
 TEST(REGISTRATION_TEST, LOGIN_TEST_FAIL) {
@@ -79,81 +78,32 @@ TEST(REGISTRATION_TEST, LOGOUT_TEST_OK_WHERE_USER_LOGOUTED) {
     EXPECT_EQ(user.logout(), 0);
 }
 
-TEST(REGISTRATION_TEST, LOGOUT_TEST_FAIL) {
-    User user;
-    user.login("command");
-    EXPECT_EQ(user.logout(), -1);
-}
-
-TEST(SERVER_TEST, GET_FROM_SERVER_TEST_OK) {
-    HTTPResponse client;
-    EXPECT_EQ(client.get(), 0);
-}
-
-TEST(SERVER_TEST, GET_FROM_SERVER_TEST_FAIL) {
-    HTTPResponse client;
-    EXPECT_EQ(client.get(), -1);
-}
 
 TEST(SERVER_TEST, SEND_ON_SERVER_TEST_OK) {
     HTTPRequest client;
-    EXPECT_EQ(client.send(), 0);
+    EXPECT_EQ(client.send(""), 0);
 }
 
 TEST(SERVER_TEST, SEND_ON_SERVER_TEST_FAIL) {
     HTTPRequest client;
-    EXPECT_EQ(client.send(), -1);
-}
-
-TEST(WORK_WITH_COMMAND_TEST, SEARCH_FILE_TEST_OK) {
-    Command client;
-    EXPECT_EQ(client.search_file("file_name"), 0);
-}
-
-TEST(WORK_WITH_COMMAND_TEST, SEARCH_FILE_TEST_FAIL) {
-    Command client;
-    EXPECT_EQ(client.search_file(""), -1);
+    EXPECT_EQ(client.send(""), -1);
 }
 
 TEST(WORK_WITH_COMMAND_TEST, WORK_WITH_FILE_TEST_OK) {
     Command client;
-    EXPECT_EQ(client.work_with_file("command"), 0);
+    EXPECT_EQ(client.upload("m@m.m file name"), 0);
 }
 
 TEST(WORK_WITH_COMMAND_TEST, WORK_WITH_FILE_TEST_FAIL) {
     Command client;
-    EXPECT_EQ(client.work_with_file(""), -1);
-}
-
-TEST(WORK_WITH_COMMAND_TEST, WORK_WITH_CHMOD_TEST_OK) {
-    Command client;
-    EXPECT_EQ(client.work_with_chmod("command"), 0);
-}
-
-TEST(WORK_WITH_COMMAND_TEST, WORK_WITH_CHMOD_TEST_FAIL) {
-    Command client;
-    EXPECT_EQ(client.work_with_chmod(""), -1);
-}
-
-TEST(WORK_WITH_COMMAND_TEST, WORK_WITH_DIRECTORY_TEST_OK) {
-    Command client;
-    EXPECT_EQ(client.work_with_directory("command"), 0);
-}
-
-TEST(WORK_WITH_COMMAND_TEST, WORK_WITH_DIRECTORY_TEST_FAIL) {
-    Command client;
-    EXPECT_EQ(client.work_with_directory(""), -1);
+    EXPECT_EQ(client.upload(""), -1);
 }
 
 TEST(ROLE_COMMAND_TEST, ROLE_COMMAND_TEST_OK) {
     Client client;
     EXPECT_EQ(client.role_command("upload"), 0);
     EXPECT_EQ(client.role_command("chmod"), 0);
-    EXPECT_EQ(client.role_command("mkdir"), 0);
     EXPECT_EQ(client.role_command("exit"), 1);
-    EXPECT_EQ(client.role_command("login"), 0);
-    EXPECT_EQ(client.role_command("logout"), 0);
-    EXPECT_EQ(client.role_command("registration"), 0);
 }
 
 TEST(ROLE_COMMAND_TEST, ROLE_COMMAND_TEST_FAIL) {
