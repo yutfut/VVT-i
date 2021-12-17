aa23a6d02f429548780548807f22bc8ac02a1caa
yut_fut <seregey.golubev@mail.ru>
2021-12-17 22:16:45 +0300

    edite headers

--- source/http_request.cpp
+++ source/http_request.cpp
@@ -27,19 +27,21 @@ std::string HTTPRequest::create_message(const std::string &email,
                            const std::string &file_name,
                            const std::string &command) {
     std::string body,
-                message = "POST / HTTP/1.0\r\n"
-                          "email: " + email + "\r\n"
-                          "password: " + password + "\r\n"
-                          "key: " + key + "\r\n"
-                          "status: \r\n"
-                          "message: \r\n"
-                          "command: " + command + "\r\n"
-                          "filename: " + file_name + "\r\n"
-                          "content-length: " + std::to_string(body.size()) + "\r\n\r\n";
+                message;
 
     if (command == "upload") {
         crate_body(file_name, body);
     }
+
+    message = "POST / HTTP/1.0\r\n"
+              "email: " + email + "\r\n"
+              "password: " + password + "\r\n"
+              "key: " + key + "\r\n"
+              "status: \r\n"
+              "message: \r\n"
+              "command: " + command + "\r\n"
+              "filename: " + file_name + "\r\n"
+              "content-length: " + std::to_string(body.size()) + "\r\n\r\n";
 
     message.append(body);
     message.append("\r\n");
