5a4fb0a7ce18e26122a8403b201bee19926f149e
kirill555101 <kir20-01@ya.ru>
2021-12-19 23:26:40 +0300

    Refactoring

---
+++ project/server_lib/http/include/http_defines.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#define GET_METHOD "GET"
+#define HEAD_METHOD "HEAD"
+
+#define NO_ROOT "no root"
+
+#define OK_STATUS 200
+#define OK_MSG "OK"
+
+#define NOT_FOUND_STATUS 404
+#define NOT_FOUND_MSG "not found"
+
+#define SERVER_HDR "Server"
+#define CONTENT_TYPE_HDR "Content-Type"
+#define CONTENT_LENGTH_HDR "Content-Length"
+#define DATE_HDR "Date"
+#define CONNECTION_HDR "Connection"
+
+#define SERVER_VL "VVT-i"
+#define CLOSE_VL "close"
