b04b619cc23af2fa3fc413410229830a0a3b0529
kirill555101 <kir20-01@ya.ru>
2021-12-25 23:46:25 +0300

    Added gzip

--- source/http_request.cpp
+++ source/http_request.cpp
@@ -4,21 +4,32 @@
 
 #include "http_request.h"
 
-void create_body(const std::string &file_name, std::string &message) {
-    char c;
-    std::ifstream in(file_name, std::ios::binary);
+#include <sstream>
+#include <boost/iostreams/filtering_streambuf.hpp>
+#include <boost/iostreams/copy.hpp>
+#include <boost/iostreams/filter/gzip.hpp>
 
-    if (!in.is_open()) {
+std::string create_body(const std::string &file_name) {
+    char c;
+    std::ifstream file(file_name, std::ios::binary);
+
+    if (!file.is_open()) {
         std::cout << "ошибка открытия файла\n";
     }
-    while (in.get(c)) {
-        message.push_back(c);
-    }
 
-    in.close();
-    if (in.is_open()) {
+    std::stringstream comp, decomp;
+
+    boost::iostreams::filtering_streambuf<boost::iostreams::input> in;
+    in.push(boost::iostreams::gzip_compressor());
+    in.push(file);
+    boost::iostreams::copy(in, comp);
+
+    file.close();
+    if (file.is_open()) {
         std::cout << "ошибка закрытия файла\n";
     }
+
+    return comp.str();
 }
 
 std::string HTTPRequest::create_message(const std::string &email,
@@ -32,7 +43,7 @@ std::string HTTPRequest::create_message(const std::string &email,
     std::string body, message;
 
     if (command == "upload") {
-        create_body(file_name, body);
+        body = create_body(file_name);
     }
 
     message = "POST / HTTP/1.0\r\n"
